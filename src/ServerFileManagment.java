c0b999d045ad24bea89622d2300f363391b1480b
Jack-Portal <jacquesantoine.portal@gmail.com>
2019-01-17 17:42:51 +0100

    Merge branch 'master' of https://github.com/Jack-Portal/SecureSoftwareULB2018
    # Conflicts:
    #	src/ClientConnectionHandler.java

--- src/ServerFileManagment.java
+++ src/ServerFileManagment.java
@@ -1,2 +1,25 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class ServerFileManagment {
+
+    public static Path getHashPath(String userName, String fileName) throws NoSuchAlgorithmException {
+        return Paths.get(Encryption.sha256(userName) + "/" + Encryption.sha256(fileName));
+    }
+
+    public void storeFile(String userName, String fileName, byte[] byteFile) throws NoSuchAlgorithmException {
+        Path userFolder = getHashPath(userName, fileName);
+    }
+
+    public byte[] loadFile(String userName, String fileName) throws NoSuchAlgorithmException {
+        Path userFolder = getHashPath(userName, fileName);
+        return null;
+    }
+
+
+
 }
