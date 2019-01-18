6d3241e1d6f152b38c1db90140d6919c26dcf739
Jack-Portal <jacquesantoine.portal@gmail.com>
2019-01-18 16:29:17 +0100

    updated the Server Command line interface for the new design where we are separating the server running and the admin user management.

--- src/ServerCommandLineInterface.java
+++ src/ServerCommandLineInterface.java
@@ -11,7 +11,13 @@ import java.util.Scanner;
 import java.net.InetAddress;
 
 /**
- * The Server class, is used to launch a server where registered users can upload, download and delete files securely.
+ * The Server Command line Interface si launched to manage the server's user.
+ * The following actions can be performed:
+ *  accept : accepting users that have sent a request to join the network.
+ *  refuse : refuse *same as above*
+ *  delete : deletes a user and its files
+ *  deactivate : moves the user to the group of frozen users (Does not delete files)
+ *  activate : moves a frozen user back into the normal user file.
  */
 public class ServerCommandLineInterface {
 
@@ -25,59 +31,6 @@ public class ServerCommandLineInterface {
     public static String AdminHashedPassword;
     public static String Salt;
 
-    /**
-     * Initialises the server
-     */
-    private ServerCommandLineInterface(){
-        //TODO ask for server address
-    }
-
-    /**
-     * Logs a user inside the server
-     */
-    private void login(){
-        //TODO login
-    }
-
-    /**
-     * Logs a user out of the server
-     */
-    private void logout(){
-        //TODO logout
-    }
-
-    private boolean accessSharedRessources(){
-        //TODO wait for other users to cooperate.
-        return false;
-    }
-
-    /**
-     * Loads a file into the client to give it to the user as a byte array.
-     * @param fileLocation The location of the file in question.
-     * @return returns the file as a byte array.
-     */
-    private byte[] loadFile(String fileLocation){
-        //TODO load the file
-        byte[] file = {};
-        //TODO handle exceptions
-        return file;
-    }
-
-    /**
-     * saves a byte array as a file on the system.
-     * @param fileName name of the file where the byte array will be saved.
-     */
-    private void saveFile(String fileName){
-        //TODO save the file
-    }
-
-    /**
-     * This function deletes a file
-     * @param fileName name of the file to be deleted.
-     */
-    private void deleteFile(String fileName){
-        //TODO SECURELY delete the file (replace all the bytes by 0 then delete it, or command to securely delete!
-    }
 
     /**
      * this function displays the pending requests and lets the admin accept them.
